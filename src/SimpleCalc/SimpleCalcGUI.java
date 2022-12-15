6c53eb398b82ea37472534ed245f8b694dc0a884
BuildTools <unconfigured@null.spigotmc.org>
2022-12-15 18:45:11 +0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -10,10 +10,13 @@ public class SimpleCalcGUI extends JFrame{
     private JTextField tfNumber;
     private JTextField lblResult;
 
+
+
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setTitle("Simple Calculator");
         app.setContentPane(app.pnlSimpleCalculator);
-        app.setSize(400, 250);
+        app.setSize(525, 250);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
     }
