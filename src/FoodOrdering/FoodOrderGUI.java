bd748e571f35913b2348d4d941e37aee62edeeab
BuildTools <unconfigured@null.spigotmc.org>
2022-12-15 19:48:07 +0800

    Add FoodOrdering main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,18 +1,29 @@
 package FoodOrdering;
 
+import LeapYear.LeapYearGUI;
+
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel pnlFoodOrdering;
-    private JCheckBox cPizza;
     private JRadioButton rbNone;
     private JButton btnOrder;
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+    private JCheckBox cPizza;
     private JCheckBox cBurger;
     private JCheckBox cFries;
     private JCheckBox cSoftDrinks;
     private JCheckBox cTea;
     private JCheckBox cSundae;
+
+    public static void main(String[] args) {
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setContentPane(app.pnlFoodOrdering);
+        app.setTitle("Food Ordering System");
+        app.setSize(450, 500);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
