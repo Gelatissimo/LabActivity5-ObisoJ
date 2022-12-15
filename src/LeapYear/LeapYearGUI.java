41dfdde46165eab980b468c8835596b6418f2b29
BuildTools <unconfigured@null.spigotmc.org>
2022-12-15 18:16:38 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,18 +1,63 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class LeapYearGUI extends JFrame{
     private JPanel pnlLeapYearChecker;
     private JTextField tfYear;
     private JButton btnCheckYear;
 
-    public static void main(String[] args) {
+    public LeapYearGUI() throws Exception{
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                LeapYearChecker();
+            }
+        });
+
+        tfYear.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    LeapYearChecker();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) throws Exception {
         LeapYearGUI app = new LeapYearGUI();
         app.setContentPane(app.pnlLeapYearChecker);
-        app.setSize(700, 700);
+        app.setSize(400, 250);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public void LeapYearChecker() {
+        try {
+            int year = Integer.parseInt(tfYear.getText());
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                JOptionPane.showMessageDialog(pnlLeapYearChecker, "Leap year");
+            } else {
+                JOptionPane.showMessageDialog(pnlLeapYearChecker, "Not a leap year");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(pnlLeapYearChecker, "Invalid input");
+        }
     }
 
 }
